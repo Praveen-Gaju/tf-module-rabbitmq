5f0b56aeb8243d5cc85a47a985366ca4bedd5b46
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-06-08 20:41:58 +0530

    updating tf-module-rabbitmq directory

--- main.tf
+++ main.tf
@@ -10,4 +10,35 @@ resource "aws_spot_instance_request" "rabbitmq" {
   )
 }
 
+resource "aws_route53_record" "" {
+  name    = ""
+  type    = ""
+  zone_id = ""
+}
 
+#security group
+resource "aws_security_group" "main" {
+  name        = "${var.env}-rabbitmq"
+  description = "${var.env}-rabbitmq"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "RABBITMQ"
+    from_port        = 5672
+    to_port          = 5672
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_subnets
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags       = merge(
+    var.tags,
+    { Name = "${var.env}-rabbitmq" }
+  )
+}
--- vars.tf
+++ vars.tf
@@ -3,3 +3,6 @@ variable "tags" {}
 variable "subnet_ids" {}
 
 variable "instance_type" {}
+
+variable "vpc_id" {}
+variable "allow_subnets" {}
